9c521957ad916c626ecf4172476b0e1cec36a99e
Yeh Swee Khim <sweekhim996@gmail.com>
2020-08-23 06:02:01 +0800

    Firebase listen for changes

--- silverkris_lounge/lib/main.dart
+++ silverkris_lounge/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:silverkris_lounge/availability.dart';
 import 'package:http/http.dart' as http;
 import 'package:firebase_database/firebase_database.dart';
 
@@ -34,18 +33,27 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final dbRef = FirebaseDatabase.instance.reference();
   var subscription;
+  var subscription1;
+  var subscription2;
+  var subscription3;
+  var subscription4;
+  var subscription5;
+  var subscription6;
+  var subscription7;
+  var subscription8;
+  var subscription9;
   final _apiGatewayURL =
       'https://mkr9pqnggb.execute-api.us-east-1.amazonaws.com/silverKris/silverKrisFunc';
-  String privRoom;
-  String restArea1;
-  String restArea2;
-  String restArea3;
-  String studyArea1;
-  String studyArea2;
-  String studyArea3;
-  String dinerRoom1;
-  String dinerRoom2;
-  String dinerRoom3;
+  static String privRoom;
+  static String restArea1;
+  static String restArea2;
+  static String restArea3;
+  static String studyArea1;
+  static String studyArea2;
+  static String studyArea3;
+  static String dinerRoom1;
+  static String dinerRoom2;
+  static String dinerRoom3;
 
   void prepStatus() {
     privRoom = "EMPTY";
@@ -62,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void readData() {
     dbRef.once().then((DataSnapshot dataSnapshot) {
-      log(dataSnapshot.value.toString());
+      log("status: " + dataSnapshot.value.toString());
       var status = dataSnapshot.value;
       setState(() {
         privRoom = checkStatus(status['privRoom']['realtime']);
@@ -79,11 +87,102 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void listenData() {
-    subscription = dbRef.onChildChanged.listen((event) {
+  void listenPrivRoom() {
+    subscription1 = dbRef.child('privRoom').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
       setState(() {
-        log(event.snapshot.key.toString());
-        log(event.snapshot.value.toString());
+        privRoom = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenRestArea1() {
+    subscription2 = dbRef.child('restArea1').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        restArea1 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenRestArea2() {
+    subscription3 = dbRef.child('restArea2').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        restArea2 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenRestArea3() {
+    subscription4 = dbRef.child('restArea3').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        restArea3 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenDinerRoom1() {
+    subscription5 = dbRef.child('dinerRoom1').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        dinerRoom1 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenDinerRoom2() {
+    subscription6 = dbRef.child('dinerRoom2').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        dinerRoom2 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenDinerRoom3() {
+    subscription7 = dbRef.child('dinerRoom3').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        dinerRoom3 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenStudyArea1() {
+    subscription8 = dbRef.child('studyArea1').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        studyArea1 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenStudyArea2() {
+    subscription9 = dbRef.child('studyArea2').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        studyArea2 = checkStatus(event.snapshot.value);
+      });
+    });
+  }
+
+  void listenStudyArea3() {
+    subscription = dbRef.child('studyArea3').onChildChanged.listen((event) {
+      log(event.snapshot.key.toString());
+      log(event.snapshot.value.toString());
+      setState(() {
+        studyArea3 = checkStatus(event.snapshot.value);
       });
     });
   }
@@ -111,26 +210,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   String checkStatus(int numPpl) {
     if (numPpl >= 10)
-      return 'HIGH';
+      return 'HIGH\n' + 'Capacity: ' + numPpl.toString();
     else if (10 > numPpl && numPpl >= 5)
-      return 'MEDIUM';
+      return 'MEDIUM\n' + 'Capacity: ' + numPpl.toString();
     else if (numPpl < 5 && numPpl > 0)
-      return 'LOW';
+      return 'LOW\n' + 'Capacity: ' + numPpl.toString();
     else
-      return 'EMPTY';
+      return 'EMPTY\n' + 'Capacity: ' + numPpl.toString();
   }
 
   @override
   void initState() {
     readData();
-    // _initStatus();
-    listenData();
+    listenPrivRoom();
+    listenDinerRoom1();
+    listenDinerRoom2();
+    listenDinerRoom3();
+    listenRestArea1();
+    listenRestArea2();
+    listenRestArea3();
+    listenStudyArea1();
+    listenStudyArea2();
+    listenStudyArea3();
     super.initState();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     subscription.cancel();
     super.dispose();
   }
@@ -139,49 +245,65 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:
-              Text('SilverKris Lounge', style: TextStyle(color: Colors.black)),
+          title: Text('SilverKris Lounge Availability',
+              style: TextStyle(color: Colors.black)),
           centerTitle: true,
           backgroundColor: Colors.white,
         ),
-        body: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/background.jpg"),
-                    fit: BoxFit.cover)),
-            child: RaisedButton(
-                child: Text('Availability'),
-                onPressed: () => Navigator.push(context,
-                        MaterialPageRoute(builder: (BuildContext context) {
-                      return AvailabilityScreen(
-                          privRoom: privRoom,
-                          dinerRoom1: dinerRoom1,
-                          dinerRoom2: dinerRoom2,
-                          dinerRoom3: dinerRoom3,
-                          restArea1: restArea1,
-                          restArea2: restArea2,
-                          restArea3: restArea3,
-                          studyArea1: studyArea1,
-                          studyArea2: studyArea2);
-                    })))));
+        body: SafeArea(
+            child: Stack(
+          children: <Widget>[
+            Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/floorplan1.png')))),
+            Container(
+                alignment: Alignment(-0.55, -0.2),
+                child: displayBox('$privRoom')),
+            Container(
+                alignment: Alignment(-0.4, 0.38),
+                child: displayBox('$dinerRoom1')),
+            Container(
+                alignment: Alignment(0.1, -0.27),
+                child: displayBox('$dinerRoom2')),
+            Container(
+                alignment: Alignment(0.12, 0.3),
+                child: displayBox('$dinerRoom3')),
+            Container(
+                alignment: Alignment(-0.8, 0.65),
+                child: displayBox('$studyArea1')),
+            Container(
+                alignment: Alignment(0.73, -0.3),
+                child: displayBox('$studyArea2')),
+            Container(
+                alignment: Alignment(-0.45, 0.9),
+                child: displayBox('$restArea1')),
+            Container(
+                alignment: Alignment(0.2, 0.9),
+                child: displayBox('$restArea2')),
+            Container(
+                alignment: Alignment(0.05, -0.8),
+                child: displayBox('$restArea3')),
+          ],
+        )));
   }
 
   Widget displayBox(String text) {
     Color color;
-    if (text == "HIGH")
+    if (text.split("\n")[0] == "HIGH")
       color = Colors.red;
-    else if (text == "MEDIUM")
+    else if (text.split("\n")[0] == "MEDIUM")
       color = Colors.yellow;
-    else if (text == "LOW")
+    else if (text.split("\n")[0] == "LOW")
       color = Colors.green;
     else
       color = Colors.green;
     return Container(
         width: 75,
-        height: 20,
+        height: 30,
         alignment: Alignment.center,
-        child: Text(text),
+        child: Text(text,
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold)),
         color: color);
   }
 }
